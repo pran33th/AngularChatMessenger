23b053d41c7c180467c38db35dafe1282e130871
pran33th <praneeth176@gmail.com>
2015-07-27 00:35:21 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name charMessengerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the charMessengerApp
+ */
+angular.module('charMessengerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name charMessengerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the charMessengerApp
+ */
+angular.module('charMessengerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
